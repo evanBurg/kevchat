ebae13bcf07a447dd7e06d869268dd1fc6d0f267
Evan Burgess <burgenator@hotmail.com>
2019-03-28 14:13:43 -0400

    PWA code to optimize for lighthouse

--- server.js
+++ server.js
@@ -12,6 +12,25 @@ let io = socketIO(server);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Enable reverse proxy support in Express. This causes the
+// the "X-Forwarded-Proto" header field to be trusted so its
+// value can be used to determine the protocol. See
+// http://expressjs.com/api#app-settings for more details.
+app.enable('trust proxy');
+// Add a handler to inspect the req.secure flag (see
+// http://expressjs.com/api#req.secure). This allows us
+// to know whether the request was via http or https.
+app.use(function (req, res, next) {
+ if (req.secure) {
+ // request was via https, so do no special handling
+ next();
+ } else {
+ // request was via http, so redirect to https
+ res.redirect('https://' + req.headers.host + req.url);
+ }
+});
+
+
 // Serve the public folder statically
 app.use(express.static('./public'));
 
