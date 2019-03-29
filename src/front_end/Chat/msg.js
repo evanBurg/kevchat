26aca621bdd65ac2deb89ef71f269b557c8e4070
Evan Burgess <burgenator@hotmail.com>
2019-03-29 13:13:16 -0400

    Embed images from messages

--- src/front_end/Chat/msg.js
+++ src/front_end/Chat/msg.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { Typography, ListItem, Collapse } from "@material-ui/core";
-import PriorityHigh from "@material-ui/icons/PriorityHigh";
+import isImageUrl from "is-image-url";
 class Msg extends React.Component {
   state = {
     open: false
   };
+
+  linkify = text => {
+    var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+    let matches = [...text.match(urlRegex)];
+    
+    debugger;
+    matches = matches.map(url => {
+      if (!isImageUrl(url)) {
+        return <React.Fragment><br/><a target="_blank" style={{clear: "both", color: 'white'}} href={url}> {url} </a></React.Fragment>;
+      } else {
+        return <a target="_blank" href={url}> <img style={{clear: "both"}} width="80%" src={url}></img> </a>;
+      }
+    });
+    return [text, ...matches]
+  };
+
   render() {
     let { message, mine, last, final } = this.props;
     return (
@@ -27,8 +43,19 @@ class Msg extends React.Component {
           }
           style={{ color: "white", background: message.color }}
         >
-          {message.from === "admin" ? <i>{message.message}</i> : message.message}
-          {message.invite && <i>{" "}<a style={{color: 'white'}} href={`/?room=${message.room}`}>Copy this!</a></i>}
+          {message.from === "admin" ? (
+            <i>{message.message}</i>
+          ) : (
+            this.linkify(message.message)
+          )}
+          {message.invite && (
+            <i>
+              {" "}
+              <a style={{ color: "white" }} href={`/?room=${message.room}`}>
+                Copy this!
+              </a>
+            </i>
+          )}
         </Typography>
         <div className={mine ? "mine details" : "yours details"}>
           <Collapse in={this.state.open}>
