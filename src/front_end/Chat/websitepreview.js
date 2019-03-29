b978ecd02620e9bae96aba0f9568ef6da491ce3d
Evan Burgess <burgenator@hotmail.com>
2019-03-29 16:26:18 -0400

    Fix link previews, make them work

--- src/front_end/Chat/websitepreview.js
+++ src/front_end/Chat/websitepreview.js
@@ -13,12 +13,14 @@ class WebsitePreview extends Component {
     loading: true,
     title: "",
     description: "",
+    show: false,
     image: false,
     url: "",
-    key: "5c9e57e18f2953cf05261987750d4bc066c5026dc7437"
+    key: "5c9e7e53b48c958f5494e7f958bdb46d4f26821bf1106"
   };
 
-  componentDidMount = async () => {
+  getData = async () => {
+    this.setState({ loading: true });
     let response = await fetch(
       `https://api.linkpreview.net/?key=${this.state.key}&q=${this.props.url}`
     );
@@ -29,40 +31,59 @@ class WebsitePreview extends Component {
     }
   };
 
-  shouldComponentUpdate(nextProps, nextState){
-      return nextProps.url !== this.props.url;
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.url !== this.props.url || this.state !== nextState;
   }
 
   getImg = () => {
-      if(this.state.image != false && this.state.image != ""){
-        return <CardMedia style={{height: 140}} image={this.state.image.replace(/^http:\/\//i, 'https://')} title={this.state.title} />
-      }else{
-          return <React.Fragment/>
-      }
-  }
+    if (this.state.image != false && this.state.image != "") {
+      return (
+        <CardMedia
+          style={{ height: 140 }}
+          image={this.state.image.replace(/^http:\/\//i, "https://")}
+          title={this.state.title}
+        />
+      );
+    } else {
+      return <React.Fragment />;
+    }
+  };
 
   render() {
     return (
-      <Collapse in={!this.state.loading}>
-        <Card style={{maxWidth: 440}}>
-          <CardActionArea>
-          {this.getImg()}
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {this.state.title}
-              </Typography>
-              <Typography component="p">{this.state.description}</Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <a style={{textDecoration: 'none'}} target="_blank" href={this.props.url}>
-              <Button size="small" color="primary">
-                Go
-              </Button>
-            </a>
-          </CardActions>
-        </Card>
-      </Collapse>
+      <React.Fragment>
+        <Button variant="outline" onClick={async () => {
+          let show = !this.state.show;
+          this.setState({show})
+          if(show){
+            this.getData();
+          }
+        }} color="primary" style={{color: 'white'}} fullWidth>Link Preview</Button>
+        <Collapse in={!this.state.loading && this.state.show}>
+          <Card style={{ maxWidth: 440 }}>
+            <CardActionArea>
+              {this.getImg()}
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {this.state.title}
+                </Typography>
+                <Typography component="p">{this.state.description}</Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <a
+                style={{ textDecoration: "none" }}
+                target="_blank"
+                href={this.props.url}
+              >
+                <Button size="small" color="primary">
+                  Go
+                </Button>
+              </a>
+            </CardActions>
+          </Card>
+        </Collapse>
+      </React.Fragment>
     );
   }
 }
