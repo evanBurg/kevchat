d79dfad67e014039b38b27e5c50d6837e1b764b2
Evan Burgess <burgenator@hotmail.com>
2019-04-08 13:39:18 -0400

    Fix logo size

--- src/front_end/Chat/logo.js
+++ src/front_end/Chat/logo.js
@@ -3,10 +3,11 @@ import { Grid } from "@material-ui/core";
 
 export default props => {
   if (props.show) {
+    const width = window.innerWidth;
     return (
       <Grid container spacing={24} justify="center" alignItems="center">
         <Grid item xs={2} style={{ textAlign: "center" }}>
-          <img src="/img/logo.png" style={{ width: "50%" }} />
+          <img src="/img/logo.png" style={{ width: width > 768 ? "50%" : '100%' }} />
         </Grid>
       </Grid>
     );
